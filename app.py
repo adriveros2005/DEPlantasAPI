1a4101a7070f68513540871a1c466f83aaeb5b60
julietxcalderon <julietxcalderon@outlook.com>
2024-10-25 01:18:35 -0300

    frontupdate3

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 tf.config.set_visible_devices([], 'GPU')
 import numpy as np
 import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 # Inicializar la app Flask
 app = Flask(__name__)
@@ -53,4 +54,4 @@ def predict():
 
 # Correr la aplicación Flask
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
